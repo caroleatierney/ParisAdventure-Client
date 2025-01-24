0269c5802cacf019531dea625e1c0704e9829144
carole tierney <caroleatierney@gmail.com>
2025-01-24 08:56:14 -0500

    homescreen change to LC in Component

--- src/routes/Home/homeScreen.jsx
+++ src/routes/Home/homeScreen.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Adrienne from "../../assets/adrienne.jpg";
 
-function HomeScreen() {
+function homeScreen() {
     return (
         <div className="flex w-1/2 mt-6 mx-auto min-h-screen">
         <img className="w-3/4 border-8 border-red-500 laptop:w-1/2 mx-auto h-full mt-5" src={Adrienne} alt={"Adrienne"} />
@@ -9,4 +9,4 @@ function HomeScreen() {
     );
     }
 
-export default HomeScreen;
+export default homeScreen;
